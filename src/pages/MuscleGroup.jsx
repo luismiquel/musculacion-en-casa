d7a9587dcde952da1482dff33f0f3c62eed0316e
Luis Miguel <luismiguelgarciadelasmorenas@gmail.com>
2025-07-30 20:25:57 +0200

    Update MuscleGroup.jsx

--- src/pages/MuscleGroup.jsx
+++ src/pages/MuscleGroup.jsx
@@ -1,12 +1,12 @@
-import { useParams } from 'react-router-dom';
+import React from "react";
 
 const MuscleGroup = () => {
-  const { group } = useParams();
-
   return (
     <div className="page">
-      <h1>Ejercicios para {group.charAt(0).toUpperCase() + group.slice(1)}</h1>
-      <p>(Aquí se mostrarán los ejercicios específicos para {group})</p>
+      <h1>Ejercicios por grupo muscular</h1>
+      <p>
+        Aquí aparecerán los ejercicios de este grupo muscular.
+      </p>
     </div>
   );
 };
