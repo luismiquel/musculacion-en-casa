51feb2c5ae20e3584a8979ff404493dc124fea9a
Luis Miguel <luismiguelgarciadelasmorenas@gmail.com>
2025-07-30 18:47:34 +0200

    Create Favorites.jsx

---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,10 @@
+const Favorites = () => {
+  return (
+    <div className="page">
+      <h1>Mis Ejercicios Favoritos</h1>
+      <p>(Aquí aparecerán los ejercicios que hayas marcado como favoritos)</p>
+    </div>
+  );
+};
+
+export default Favorites;
